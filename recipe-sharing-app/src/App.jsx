e4260ebf21270d2c494e04bd38b49da1c164abc9
Taiwo Afusat <taiwo.asat@gmail.com>
2025-12-02 23:31:01 +0100

    Initialize Recipe Sharing App with Zustand, add components and store

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+function App() {
+  return (
+    <div style={{ maxWidth: 800, margin: "40px auto", padding: "0 20px" }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
