538c2f940d3b10307cd0226fb867ef2f6125bb7e
Taiwo Afusat <taiwo.asat@gmail.com>
2025-12-02 23:50:05 +0100

    ALX Task 1: Enhance store with update and delete functionality

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,12 +1,21 @@
-// src/store/recipeStore.js
 import { create } from "zustand";
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
 
   addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  updateRecipe: (updatedRecipe) =>
     set((state) => ({
-      recipes: [...state.recipes, newRecipe],
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 
   setRecipes: (recipes) => set({ recipes }),
